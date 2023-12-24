35d0f125ec5b8dc1a9e36ad9b7f3f2923180de6d
Oleksii Tereshchenko <o.tereshchenko@spd.tech>
2023-12-24 19:23:24 +0200

    fix git hooks script

--- plugin/src/main/resources/git-hooks/pre-push.sh
+++ plugin/src/main/resources/git-hooks/pre-push.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [[ $(git status --porcelain | grep api/src) ]]; then
   git stash -q --keep-index
-  ./gradlew api:test
+  ./gradlew test
   ./gradlew clean build -x test
   status=$?
   git stash pop -q
   exit $status
-fi

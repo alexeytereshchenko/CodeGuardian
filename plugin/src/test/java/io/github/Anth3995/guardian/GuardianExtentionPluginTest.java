a45fff38a231e62c6f00f1a106888e2d3b45849d
Oleksii Tereshchenko <o.tereshchenko@spd.tech>
2023-12-24 21:49:37 +0200

    add descriptions and tags for publishing

--- plugin/src/test/java/io/github/Anth3995/guardian/GuardianExtentionPluginTest.java
+++ plugin/src/test/java/io/github/Anth3995/guardian/GuardianExtentionPluginTest.java
@@ -8,14 +8,11 @@ import org.gradle.api.Project;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * A simple unit test for the 'tech.spd.spacehub.guardian.greeting' plugin.
- */
 class GuardianExtentionPluginTest {
     @Test void pluginRegistersATask() {
         // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
-        project.getPlugins().apply("tech.spd.spacehub.guardian");
+        project.getPlugins().apply("io.github.Anth3995.guardian");
 
         // Verify the result
         assertNotNull(project.getTasks().findByName("greeting"));
